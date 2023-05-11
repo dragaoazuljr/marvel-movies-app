eb9fa11f379c20ee4055b08b9af1f9d7db2b1abe
Danillo Moraes <danillo.moraes@outlook.com>
2023-05-11 16:49:45 -0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'marvel-movies-app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'marvel-movies-app';
+}
